199578ecccf3dd691026db71bf1eb1a57f412947
ekinkarakoc <ekinkarakoc66@gmail.com>
2024-02-20 17:08:04 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows of the array: ");
+        int row = sc.nextInt();
+        System.out.print("Enter the number of column of the array: ");
+        int col = sc.nextInt();
+
+        MineSwepper mineSwepper = new MineSwepper(row,col);
+        mineSwepper.run();
+
+    }
+}
